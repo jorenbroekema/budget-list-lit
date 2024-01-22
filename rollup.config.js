171254f5bd80a0a5c04c068e4abc6ffeb67a9883
jorenbroekema <joren.broekema@gmail.com>
2024-01-22 22:04:08 +0100

    chore: make it vanilla javascript only

--- rollup.config.js
+++
@@ -1,9 +0,0 @@
-import { rollupPluginHTML as html } from '@web/rollup-plugin-html';
-import { nodeResolve } from '@rollup/plugin-node-resolve';
-import { importMetaAssets } from '@web/rollup-plugin-import-meta-assets';
-
-export default {
-  input: 'index.html',
-  output: { dir: 'dist' },
-  plugins: [html(), nodeResolve(), importMetaAssets()],
-};
